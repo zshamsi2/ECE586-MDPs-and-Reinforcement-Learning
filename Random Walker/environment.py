2e7751573e7459b7237f5aa796df722c09793c81
Zahra Shamsi <zshamsi2@illinois.edu>
2017-12-13 11:34:07 -0600

    Update environment.py

--- Random Walker/environment.py
+++ Random Walker/environment.py
@@ -200,15 +200,12 @@ class mockSimulation:
                         fig.canvas.draw()
                 return trj_x, trj_y          
 
-# output size :
-# 2 * simu length * number of parallel sims
-
         def pltFinalPoints(self, trjs_theta):
                 import numpy as np
                 import matplotlib.pyplot as plt
-                plt.rcParams.update({'font.size':20})
-                plt.rc('xtick', labelsize=20)
-                plt.rc('ytick', labelsize=20)
+                plt.rcParams.update({'font.size':18})
+                plt.rc('xtick', labelsize=18)
+                plt.rc('ytick', labelsize=18)
                 x = np.array(trjs_theta[0])
                 y = np.array(trjs_theta[1])
 
@@ -217,8 +214,8 @@ class mockSimulation:
                 bb = [0, 0, 0, 0, 0] # radius in xy
                 cc = [-20, -20, -2, -20, -20] # inverse radius in y
                 AA = 3*[-200, -120, -200, -80, -80] # strength
-                XX = [1, 0, 0, 0, 0.4] # center_x
-                YY = [0, 0, 1, 0.4, 0] # center_y
+                XX = [1.3, 0.3, 0.3, 0.3, 0.7] 
+                YY = [0.35, 0.35, 1.35, 0.75, 0.35]
                 zxx = np.mgrid[-1:2.51:0.01]
                 zyy = np.mgrid[-1:2.51:0.01]
                 xx, yy = np.meshgrid(zxx, zyy)
@@ -230,6 +227,8 @@ class mockSimulation:
                 ax.contourf(xx,yy,np.minimum(V1,200), 40)
                 plt.xlabel('x')
                 plt.ylabel('y')
+                plt.ylim([0, 2])
+                plt.xlim([0, 2])
                 plt.plot(x, y, 'o', color='white', alpha=0.2, mec="black")
                 plt.savefig('fig_all.png', dpi =500)
                 
