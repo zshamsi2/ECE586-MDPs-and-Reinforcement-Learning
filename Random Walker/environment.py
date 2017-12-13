ec6ccb53860fa4e4acca1960e5b49f00286ab57b
Zahra Shamsi <zshamsi2@illinois.edu>
2017-12-13 11:30:28 -0600

    Update environment.py

--- Random Walker/environment.py
+++ Random Walker/environment.py
@@ -61,39 +61,23 @@ class mockSimulation:
         def findStarting(self, trj_Sp_theta, trj_Sp, W_1, starting_n=10 , method = 'RL'):
                 return newPoints
 
-        def run_noPlt(self, inits, nstepmax = 10):
+        def run(self, inits, nstepmax = 10):
                 import numpy as np
                 import time 
                 from scipy.interpolate import interp1d
                 inits_x = inits[0]
                 inits_y = inits[1]                    
-                #plt.ion()
-                # max number of iterations
-                
-                # frequency of plotting
-                #nstepplot = 1e1
-                # plot string every nstepplot if flag1 = 1 
-                #flag1 = 1
-
-                # temperature ###?!!!
-                mu=9
+                # temperature
                 mu = 3
                 # parameter used as stopping criterion
                 tol1 = 1e-7
-
-                # number of images during prerelaxation
-                n2 = 1e1;
-                # number of images along the string (try from  n1 = 3 up to n1 = 1e4)
-                n1 = 25
+                n2 = 1e1
                 n1 = len(inits_x)
-                # time-step (limited by the ODE step on line 83 & 84 but independent of n1)
-                h = 1e-4
-                # end points of the initial string
-                # notice that they do NOT have to be at minima of V -- the method finds
-                # those automatically
-
-                # initialization
                 
+                # time-step (limited by the ODE step)
+                h = 1e-4
+
+                # initialization                
                 x = np.array(inits_x)
                 y = np.array(inits_y)
                 dx = x-np.roll(x, 1)
@@ -102,7 +86,6 @@ class mockSimulation:
                 dy[0] = 0
                 xi = x
                 yi = y
-
                 # parameters in Mueller potential
                 aa = [-2, -20, -20, -20, -20] # inverse radius in x
                 bb = [0, 0, 0, 0, 0] # radius in xy
@@ -111,20 +94,16 @@ class mockSimulation:
 
                 XX = [1, 0, 0, 0, 0.4] # center_x
                 YY = [0, 0, 1, 0.4, 0] # center_y
-
                 zxx = np.mgrid[-1:2.51:0.01]
                 zyy = np.mgrid[-1:2.51:0.01]
                 xx, yy = np.meshgrid(zxx, zyy)
-
                 V1 = AA[0]*np.exp(aa[0] * np.square(xx-XX[0]) + bb[0] * (xx-XX[0]) * (yy-YY[0]) +cc[0]*np.square(yy-YY[0]))
                 for j in range(1,5):
                         V1 =  V1 + AA[j]*np.exp(aa[j]*np.square(xx-XX[j]) + bb[j]*(xx-XX[j])*(yy-YY[j]) + cc[j]*np.square(yy-YY[j]))
-
                 ##### Main loop
                 trj_x = []
                 trj_y = []
                 for nstep in range(int(nstepmax)):
-                        
                         # calculation of the x and y-components of the force, dVx and dVy respectively
                         ee = AA[0]*np.exp(aa[0]*np.square(x-XX[0])+bb[0]*(x-XX[0])*(y-YY[0])+cc[0]*np.square(y-YY[0]))
                         dVx = (aa[0]*(x-XX[0])+bb[0]*(y-YY[0]))*ee
@@ -139,10 +118,9 @@ class mockSimulation:
                         y = y - h*dVy + np.sqrt(h*mu)*np.random.randn(1,n1)
                         trj_x.append(x) 
                         trj_y.append(y)
-                        
                 return trj_x, trj_y    
 
-        def run(self, inits, nstepmax = 10):
+        def run_Plt(self, inits, nstepmax = 10):
                 import numpy as np
                 import time 
                 from scipy.interpolate import interp1d
@@ -157,23 +135,15 @@ class mockSimulation:
                 # plot string every nstepplot if flag1 = 1 
                 flag1 = 1
 
-                # temperature ###?!!!
-                mu=9
+                # temperature 
                 mu =3
                 # parameter used as stopping criterion
                 tol1 = 1e-7
-
-                # number of images during prerelaxation
                 n2 = 1e1;
-                # number of images along the string (try from  n1 = 3 up to n1 = 1e4)
-                n1 = 25
                 n1 = len(inits_x)
-                # time-step (limited by the ODE step on line 83 & 84 but independent of n1)
+                # time-step 
                 h = 1e-4
-                # end points of the initial string
-                # notice that they do NOT have to be at minima of V -- the method finds
-                # those automatically
-
+                
                 # initialization
                 x = np.array(inits_x)
                 y = np.array(inits_y)
@@ -199,19 +169,16 @@ class mockSimulation:
                 V1 = AA[0]*np.exp(aa[0] * np.square(xx-XX[0]) + bb[0] * (xx-XX[0]) * (yy-YY[0]) +cc[0]*np.square(yy-YY[0]))
                 for j in range(1,5):
                         V1 =  V1 + AA[j]*np.exp(aa[j]*np.square(xx-XX[j]) + bb[j]*(xx-XX[j])*(yy-YY[j]) + cc[j]*np.square(yy-YY[j]))
-
                 fig = plt.figure()
                 ax = fig.add_subplot(111)
                 plt.axvline(x=self.theta_mean[0])
                 plt.axhline(y=self.theta_mean[1])
                 plt.xlabel('x')
                 plt.ylabel('y')
-
                 ##### Main loop
                 trj_x = []
                 trj_y = []
                 for nstep in range(int(nstepmax)):
-                        
                         # calculation of the x and y-components of the force, dVx and dVy respectively
                         ax.contourf(xx,yy,np.minimum(V1,200), 40)
                         ee = AA[0]*np.exp(aa[0]*np.square(x-XX[0])+bb[0]*(x-XX[0])*(y-YY[0])+cc[0]*np.square(y-YY[0]))
@@ -255,7 +222,6 @@ class mockSimulation:
                 zxx = np.mgrid[-1:2.51:0.01]
                 zyy = np.mgrid[-1:2.51:0.01]
                 xx, yy = np.meshgrid(zxx, zyy)
-
                 V1 = AA[0]*np.exp(aa[0] * np.square(xx-XX[0]) + bb[0] * (xx-XX[0]) * (yy-YY[0]) +cc[0]*np.square(yy-YY[0]))
                 for j in range(1,5):
                         V1 =  V1 + AA[j]*np.exp(aa[j]*np.square(xx-XX[j]) + bb[j]*(xx-XX[j])*(yy-YY[j]) + cc[j]*np.square(yy-YY[j]))
@@ -266,5 +232,4 @@ class mockSimulation:
                 plt.ylabel('y')
                 plt.plot(x, y, 'o', color='white', alpha=0.2, mec="black")
                 plt.savefig('fig_all.png', dpi =500)
-
-
+                
