323084e54adbb62e87ff22884c8d12c00f0596f5
Zahra Shamsi <zshamsi2@illinois.edu>
2017-12-14 01:57:54 -0600

    Update walkerSimEnv.py

--- Random Walker/walkerSimEnv.py
+++ Random Walker/walkerSimEnv.py
@@ -311,7 +311,7 @@ class walkerSimulation:
                         fig.canvas.draw()
                 return trj_x, trj_y          
 
-        def pltFinalPoints(self, trjs_theta):
+        def pltFinalPoints(self, trjs_theta, num):
                 import numpy as np
                 import matplotlib.pyplot as plt
                 plt.rcParams.update({'font.size':18})
@@ -324,11 +324,13 @@ class walkerSimulation:
                 aa = [-2, -20, -20, -20, -20] # inverse radius in x
                 bb = [0, 0, 0, 0, 0] # radius in xy
                 cc = [-20, -20, -2, -20, -20] # inverse radius in y
-                AA = 3*[-200, -120, -200, -80, -80] # strength
-                XX = [1.3, 0.3, 0.3, 0.3, 0.7] 
-                YY = [0.35, 0.35, 1.35, 0.75, 0.35]
+                AA = 30*[-200, -120, -200, -80, -80] # strength
+
+                XX = [1, 0, 0, 0, 0.4] # center_x
+                YY = [0, 0, 1, 0.4, 0] # center_y
                 zxx = np.mgrid[-1:2.51:0.01]
                 zyy = np.mgrid[-1:2.51:0.01]
+                
                 xx, yy = np.meshgrid(zxx, zyy)
                 V1 = AA[0]*np.exp(aa[0] * np.square(xx-XX[0]) + bb[0] * (xx-XX[0]) * (yy-YY[0]) +cc[0]*np.square(yy-YY[0]))
                 for j in range(1,5):
@@ -341,5 +343,5 @@ class walkerSimulation:
                 plt.ylim([0, 2])
                 plt.xlim([0, 2])
                 plt.plot(x, y, 'o', color='white', alpha=0.2, mec="black")
-                plt.savefig('fig_all.png', dpi =500)
+                plt.savefig('fig_'+str(num)+'.png', dpi =500)
                 
